b2637ed6fb54d46a8fcc6f2ae3c25093d321b521
mabaranowski <mateusz.m.baranowski@gmail.com>
2019-12-29 17:14:19 +0100

    toggle switch and relay swap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'switchboxd-controller';
+export class AppComponent implements OnInit {
+
+  private relay: Number = 0;
+
+  constructor(private service: AppService) { }
+
+  @HostListener('document:keydown.space')
+  changeRelay() {
+    this.relay === 0 ? this.relay = 1 : this.relay = 0;
+    console.log(this.relay);
+  }
+
+  ngOnInit() {
+    this.service.setRelayState(this.relay, 0).subscribe(resp => {
+      console.log(resp);
+    })
+  }
+
+  toggleSwitch() {
+    this.service.setRelayState(this.relay, 2).subscribe(resp => {
+      console.log(resp);
+    })
+
+    // this.service.getDeviceStatus().subscribe(resp => {
+    //   console.log(resp);
+    // });
+  }
+
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AppService {
+    private ip = 'http://192.168.8.50/';
+
+    constructor(private http: HttpClient) { }
+
+    setRelayState(relay, state) {
+        return this.http.get(`${this.ip}s/${relay}/${state}`);
+    }
+
+    getDeviceStatus() {
+        return this.http.get(`${this.ip}api/device/state`);
+    }
+
+}
